41016538c1cbceb6d36819828535944c8addbf67
0Calories <cppanand@gmail.com>
2019-01-16 19:46:34 -0500

    Add styling to Searchbar component

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -8,7 +8,7 @@ export default class Searchbar extends React.Component {
       <div className="searchbar">
         <form>
           <input className="searchbar__input" type="text" name="search" />
-          <input className="searchbar__button" type="image" />
+          <input className="searchbar__button" type="image" src="/images/search-icon.png" />
         </form>
       </div>
     )
