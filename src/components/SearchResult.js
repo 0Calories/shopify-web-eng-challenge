9381902779c555e541be54a51e23e79b0cc1be34
0Calories <cppanand@gmail.com>
2019-01-20 18:58:07 -0500

    Add styling for SearchResult component

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -9,6 +9,7 @@ export default class SearchResult extends React.Component {
     body: ''
   };
 
+  // Called whenever the props are updated for this SearchResult component, or a new SearchResult is created
   static getDerivedStateFromProps(props, state) {
     if (props.body !== state.body) {
       // Convert the body text into HTML so it can be directly set in the render method
@@ -23,8 +24,12 @@ export default class SearchResult extends React.Component {
   render() {
 
     return (
-      <div>
-        <div className="result-title">{this.props.title}</div>
+      <div className="result-container">
+        <i class="fas fa-star"></i>
+        <div className="result-title">
+          
+          {this.props.title}
+        </div>
         <div 
           className="result-body"
           dangerouslySetInnerHTML={{ __html: this.state.body }}
