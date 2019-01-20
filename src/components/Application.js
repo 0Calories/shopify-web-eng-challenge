3a682df7bab80200ba01ca62704466ad62457d4f
0Calories <cppanand@gmail.com>
2019-01-20 13:24:00 -0500

    Use ES6 filter method for search functionality

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,13 +4,22 @@ import Searchbar from './Searchbar';
 
 // In my solution, I chose to save the JSON file provided by the Waste Wizard website.
 // The data will be directly parsed, so no need for API calls
-import recyclingData from './../database/data.json'; 
+import lookupData from './../database/data.json'; 
 
 export default class Application extends React.Component {
 
+  state = {
+    lookupData
+  };
+
   handleSearch = (searchInput) => {
-    console.log(searchInput);
-  }
+    // Use the ES6 filter method on the lookupData.
+    // This works by taking all objects where 'searchInput' is found in the 'title' or 'keywords' properties.
+    const searchResults = this.state.lookupData.filter((item) => 
+      item.keywords.includes(searchInput) || item.title.includes(searchInput)
+    );
+    console.log(searchResults);
+  };
 
   render() {
 
