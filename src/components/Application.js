b4f3ab9d49586b9f20891fc2e4ca54b3e55c1694
0Calories <cppanand@gmail.com>
2019-01-20 12:43:22 -0500

    Add basic functionality for search form and add lookup data JSON to project

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 import Searchbar from './Searchbar';
 
+// In my solution, I chose to save the JSON file provided by the Waste Wizard website.
+// The data will be directly parsed, so no need for API calls
+import recyclingData from './../database/data.json'; 
+
 export default class Application extends React.Component {
 
+  handleSearch = (searchInput) => {
+    console.log(searchInput);
+  }
+
   render() {
+
     return (
       <div>
         <div className="header">
           Toronto Waste Lookup
         </div>
-        <Searchbar />
+        <Searchbar onSearch={this.handleSearch} />
         Hello World!
       </div>
     )
--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,14 +1,37 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 export default class Searchbar extends React.Component {
+
+  state = {
+    searchInput: undefined,
+    favourites: []
+  };
+
+  handleSearch = e => {
+    e.preventDefault();
+    this.props.onSearch(this.state.searchInput);
+  }
+
+  handleChangeSearchInput = e => {
+    this.setState({ searchInput: e.target.value });
+  };
 
   render() {
     return (
       <div className="searchbar">
         <form>
-          <input className="searchbar__input" type="text" name="search" />
-          <input className="searchbar__button" type="image" src="/images/search-icon.png" />
+          <input 
+            className="searchbar__input" 
+            type="text" 
+            name="search" 
+            onChange={this.handleChangeSearchInput}
+          />
+          <input 
+            className="searchbar__button" 
+            type="image" 
+            src="/images/search-icon.png" 
+            onClick={this.handleSearch}
+          />
         </form>
       </div>
     )
